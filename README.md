9675dabbce7f10a9ed9286d58ec46075a955ded1
Matt Chan <mattchan@awakenetworks.com>
2016-08-28 18:50:26 -0700

    ugh

--- README.md
+++ README.md
@@ -5,8 +5,19 @@ React.js Fundamentals Curriculum
 
 ####For more information on React.js Program, [click here](http://reactjsprogram.com)
 
+## Building
+
+```
+bower install
+npm install
+
+npm run build
+
+open public/index.html
+```
+
 ##Objective
-Build a functioning Weather app using the [Open Weather API](http://openweathermap.org/api). The end result of this project can be found [HERE](http://www.reactjsprogram.com/React-Fundamentals-Project). 
+Build a functioning Weather app using the [Open Weather API](http://openweathermap.org/api). The end result of this project can be found [HERE](http://www.reactjsprogram.com/React-Fundamentals-Project).
 
 ## Notes
 The goal here is to give you just enough guidance for you to struggle without drowning. Note that the steps below are just suggestions. The ideal situation is you look at the completed project, then you build it. However, if you're not up for such things, feel free to follow the (vague by design) steps below. If you get stuck, all steps have coinciding branches for you to reference as a last case scenario.
--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,37 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import React.DOM as D
+import React.DOM.Props as P
+import Container                     (container)
+import Control.Monad.Eff             (Eff)
+import Control.Monad.Eff.Console     (log)
+import DOM                           (DOM())
+import DOM.HTML                      (window)
+import DOM.HTML.Types                (htmlDocumentToDocument)
+import DOM.HTML.Window               (document)
+import DOM.Node.NonElementParentNode (getElementById)
+import DOM.Node.Types                (Element, ElementId(..),
+                                      documentToNonElementParentNode)
+import Data.Int                      (decimal, toStringAs)
+import Data.Maybe                    (fromJust)
+import Data.Nullable                 (toMaybe)
+import Partial.Unsafe                (unsafePartial)
+import React                         (ReactElement, ReactClass,
+                                      createElement, createFactory, createClass,
+                                      createClassStateless,
+                                      writeState, readState, spec, getProps)
+import ReactDOM                      (render)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+hello :: forall props. ReactClass { name :: String | props }
+hello = createClass $ spec unit \ctx -> do
+  props <- getProps ctx
+  pure $ D.h1 [ P.className "Hello"
+              , P.style { background: "lightgray" }
+              ]
+              [ D.text "Hello, "
+              , D.text props.name
+              , createElement (createClassStateless \props' ->
+                                D.div' [ D.text $ "Stateless" <> props'.test ])
+                                { test: " test" } []
+              ]
